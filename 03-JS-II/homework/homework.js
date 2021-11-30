77c390311388e4ff735255bb601d12b983f61ccb
BNSTNZCR <blacksmith08@hotmail.com>
2021-11-30 12:02:07 -0500

    30 11 21/ 12 00 pm

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -116,6 +116,10 @@ function esEntero(numero) {
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
 
+  if (numero === 0){
+    return true
+  }
+
   if (numero / Math.floor(numero) === 1){
     return true
   }
@@ -144,11 +148,30 @@ function fizzBuzz(numero) {
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //   Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //   Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //   0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  // 80, -1, 2
+
+   if (num1 === 0 || num2 === 0 || num3 === 0){
+     return 'Error'
+   }
+   if (num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos"
+   }
+   if (num1 > num2 && num1 > num3 && num1 > 0){
+     return "Número 1 es mayor y positivo"
+   }
+   
+   if (num3 > num1 && num3 > num2) {
+     num3 = num3 + 1
+     return num3
+   }
+   return false
+
+
 }
 
 function esPrimo(numero) {
